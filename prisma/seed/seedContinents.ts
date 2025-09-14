0a17c2fcb37065e2aaa08cdd63376247f06db622
MR0808 <kram@grebnesor.com>
2025-09-14 21:58:53 +1000

    seed

--- prisma/seed/seedContinents.ts
+++ prisma/seed/seedContinents.ts
@@ -1,39 +1,39 @@
-import { PrismaClient } from '@prisma/client';
-const continents = [
-    'Asia',
-    'Europe',
-    'Africa',
-    'Oceania',
-    'North America',
-    'South America',
-    'Antarctica'
-];
-const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// const continents = [
+//     'Asia',
+//     'Europe',
+//     'Africa',
+//     'Oceania',
+//     'North America',
+//     'South America',
+//     'Antarctica'
+// ];
+// const prisma = new PrismaClient();
 
-async function seedContinents() {
-    const totalLength = continents.length;
-    let count = 1;
-    try {
-        console.log('🌎 Seeding continents...');
-        for (const continent of continents) {
-            await prisma.continent.create({
-                data: {
-                    name: continent
-                }
-            });
-            console.log(`Seeded ${count} / ${totalLength} continents`);
-            count++;
-        }
-        console.log(`✅ Seeded ${continents.length} continents`);
-    } catch (error) {
-        console.error('❌ Error during seeding:', error);
-        throw error;
-    }
-}
+// async function seedContinents() {
+//     const totalLength = continents.length;
+//     let count = 1;
+//     try {
+//         console.log('🌎 Seeding continents...');
+//         for (const continent of continents) {
+//             await prisma.continent.create({
+//                 data: {
+//                     name: continent
+//                 }
+//             });
+//             console.log(`Seeded ${count} / ${totalLength} continents`);
+//             count++;
+//         }
+//         console.log(`✅ Seeded ${continents.length} continents`);
+//     } catch (error) {
+//         console.error('❌ Error during seeding:', error);
+//         throw error;
+//     }
+// }
 
-seedContinents()
-    .catch((error) => {
-        console.error('💥 Seed failed:', error);
-        process.exit(1);
-    })
-    .finally(() => prisma.$disconnect());
+// seedContinents()
+//     .catch((error) => {
+//         console.error('💥 Seed failed:', error);
+//         process.exit(1);
+//     })
+//     .finally(() => prisma.$disconnect());
--- prisma/seed/seedIndustries.ts
+++ prisma/seed/seedIndustries.ts
@@ -1,54 +1,54 @@
-import { PrismaClient } from '@prisma/client';
-import GithubSlugger from 'github-slugger';
+// import { PrismaClient } from '@prisma/client';
+// import GithubSlugger from 'github-slugger';
 
-const industries = [
-    'Technology',
-    'Healthcare',
-    'Finance',
-    'Retail',
-    'Manufacturing',
-    'Professional Services',
-    'Education',
-    'Hospitality',
-    'Construction',
-    'Non-Profit / NGO',
-    'Media / Entertainment',
-    'Transportation / Logistics',
-    'Government / Public Sector',
-    'Energy / Utilities',
-    'Other'
-];
+// const industries = [
+//     'Technology',
+//     'Healthcare',
+//     'Finance',
+//     'Retail',
+//     'Manufacturing',
+//     'Professional Services',
+//     'Education',
+//     'Hospitality',
+//     'Construction',
+//     'Non-Profit / NGO',
+//     'Media / Entertainment',
+//     'Transportation / Logistics',
+//     'Government / Public Sector',
+//     'Energy / Utilities',
+//     'Other'
+// ];
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const slugger = new GithubSlugger();
+// const slugger = new GithubSlugger();
 
-async function seedIndustries() {
-    const totalLength = industries.length;
-    let count = 1;
-    try {
-        console.log('Seeding industries...');
-        for (const industry of industries) {
-            let slug = slugger.slug(industry);
-            await prisma.industry.create({
-                data: {
-                    slug,
-                    name: industry
-                }
-            });
-            console.log(`Seeded ${count} / ${totalLength} industries`);
-            count++;
-        }
-        console.log(`✅ Seeded ${industries.length} industries`);
-    } catch (error) {
-        console.error('❌ Error during seeding:', error);
-        throw error;
-    }
-}
+// async function seedIndustries() {
+//     const totalLength = industries.length;
+//     let count = 1;
+//     try {
+//         console.log('Seeding industries...');
+//         for (const industry of industries) {
+//             let slug = slugger.slug(industry);
+//             await prisma.industry.create({
+//                 data: {
+//                     slug,
+//                     name: industry
+//                 }
+//             });
+//             console.log(`Seeded ${count} / ${totalLength} industries`);
+//             count++;
+//         }
+//         console.log(`✅ Seeded ${industries.length} industries`);
+//     } catch (error) {
+//         console.error('❌ Error during seeding:', error);
+//         throw error;
+//     }
+// }
 
-seedIndustries()
-    .catch((error) => {
-        console.error('💥 Seed failed:', error);
-        process.exit(1);
-    })
-    .finally(() => prisma.$disconnect());
+// seedIndustries()
+//     .catch((error) => {
+//         console.error('💥 Seed failed:', error);
+//         process.exit(1);
+//     })
+//     .finally(() => prisma.$disconnect());
